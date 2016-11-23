0d92223f109873e8f3247723c1270767491db1e1
Nikoleta <nvkoleva@yahoo.com>
2016-11-23 07:46:55 -0500

    add header_guards + delete vs header file

---
+++ chapter_1/ch1.10a_header guards/ch1.10a_header guards_main.cpp
@@ -0,0 +1,11 @@
+// ch1.10a_header guards.cpp : this is the main.cpp
+//
+
+#include "stdafx.h"
+#include "ch1.10a_header guards_math.h"
+#include "ch1.10a_header guards_geometry.h"
+
+int main()
+{
+	return 0;
+}
---
+++ chapter_1/ch1.10a_header guards/ch1.10a_header guards_math.h
@@ -0,0 +1,9 @@
+#ifndef CH1_10A_HEADER_GUARDS_MATH_H
+#define CH1_10A_HEADER_GUARDS_MATH_H
+
+int getSquareSides()
+{
+	return 4;
+}
+
+#endif
