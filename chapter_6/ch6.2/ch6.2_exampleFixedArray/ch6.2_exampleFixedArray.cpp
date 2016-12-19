3030ff6abec0cee7b79b5327b3823ac4bdcaec86
Nikoleta <nvkoleva@yahoo.com>
2016-12-19 17:07:20 -0500

    ch6.2 fixed array quizzes

---
+++ chapter_6/ch6.2/ch6.2_exampleFixedArray/ch6.2_exampleFixedArray.cpp
@@ -0,0 +1,14 @@
+// ch6.2_exampleFixedArray.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	double highestTemp[365]{ 0.0 };
+	std::cout << highestTemp;
+	return 0;
+}
+
